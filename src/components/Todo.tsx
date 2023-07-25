965d9be2f1dc17ed5405a82f4b65cfcff205fe1c
Kevin Barreto <barretokevin@hotmail.com>
2023-07-25 17:38:33 -0600

    rename file

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import ITodo from "../interfaces/Todo";
+import ITodo from "../interfaces/ITodo";
 
 function Todo() {
   const [data, setData] = useState<ITodo[]>([]);
